beef5e38cfab2bd7b7112e3a406fbfcbf2f7348f
krunal04111993 <107112713+krunal04111993@users.noreply.github.com>
2023-04-20 12:43:21 +0530

    Update server.js
    dwdwd

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const auth = (req,res,  next) => {
 	next();
 }
 app.get('/',  (req, res) => {
-	res.send('tredtdferfjferjfejbbf');
+	res.send('sssssssss');
   })
 app.get('/testgetrequest',  (req, res) => {
 	res.send('Hello World!');
