a77905c6a1de939008d9a95a1441c6483e8b0cbc
krunal04111993 <107112713+krunal04111993@users.noreply.github.com>
2023-04-20 11:48:10 +0530

    Update server.js
    commited

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express')//
-const app = express()
+const express = require('express');
+const app = express();
 //const bodyParser = require('body-parser');
 //app.use(bodyParser);
 // create application/json parser
